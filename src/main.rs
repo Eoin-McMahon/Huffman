ae2ccdb4c786a4037be16a1c5134ce8b6c5db977
Eoin McMahon <eoin.mcmahon.dev@gmail.com>
2020-05-21 18:01:11 +0100

    Added frequency table

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+use std::collections::HashMap;
+
+
+fn frequency_map(s: &str) -> HashMap<char, i32> {
+    let mut h = HashMap::new();
+    for ch in s.chars() {
+        let count = h.entry(ch).or_insert(0); // get count from hashmap otherwise add 0
+        *count += 1;
+    }
+
+    return h;
+}
+
+
 fn main() {
-    println!("Hello, world!");
+    let freqs = frequency_map("eoin mcmahon");
+    println!("{:?}", freqs);
 }
